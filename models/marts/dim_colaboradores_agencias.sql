b4795b7f9932d282d00472fd992365d414b363b8
gilberto <ant.gilbertosantos@gmail.com>
2025-02-07 20:09:39 -0300

    alteração na ref da tabela, estava pegando uma referencia errada

---
+++ models/marts/dim_colaboradores_agencias.sql
@@ -0,0 +1,29 @@
+with 
+    agencias as (
+        select *
+        from{{ ref('stg_agencia') }}
+    )
+    , colaboradores_agencias as (
+        select *
+        from {{ ref('stg_colaborador_agencia') }}
+    )
+    , colaboradores as (
+        select *
+        from {{ ref('stg_colaborador') }}
+    )
+    , dim_colaboradores_agencias as (
+        select 
+            agencias.agencia_id
+            , colaboradores.colaborador_id
+            , agencias.nome as nome_agencia
+            , agencias.cidade
+            , agencias.uf
+            , agencias.tipo_agencia
+            , colaboradores.nome_completo
+            , colaboradores.idade
+        from colaboradores_agencias
+        left join agencias on colaboradores_agencias.agencia_id = agencias.agencia_id
+        left join colaboradores on colaboradores_agencias.colaborador_id = colaboradores.colaborador_id
+    )
+select *
+from dim_colaboradores_agencias
