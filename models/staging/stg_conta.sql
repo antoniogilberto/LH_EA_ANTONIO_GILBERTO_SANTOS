293fce3ce7efd10ef7adf8beae382c58fe5ddbea
gilberto <ant.gilbertosantos@gmail.com>
2025-02-07 21:56:02 -0300

    foi alterado a coluna contas_id para conta_id

--- models/staging/stg_conta.sql
+++ models/staging/stg_conta.sql
@@ -1,13 +1,13 @@
 with 
     stg_contas as (
         select
-            num_conta as contas_id
+            num_conta as conta_id
             , cod_cliente as cliente_id
             , cod_agencia as agencia_id
             , cod_colaborador as colaborador_id
             , case 
                 when tipo_conta = 'PF' then 'Pessoa Física' end as tipo_contas
-            , cast(data_abertura as date) as data_abertura
+            , date(data_abertura) as data_abertura
             , saldo_total
             , saldo_disponivel
             , cast(data_ultimo_lancamento as date) as data_ultimo_lancamento
