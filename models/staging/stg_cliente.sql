f3d2b0ae45798c30220ec74b5ae3f61efdc4cecb
gilberto <ant.gilbertosantos@gmail.com>
2025-02-08 15:23:19 -0300

    add faixa_etaria para idade

--- models/staging/stg_cliente.sql
+++ models/staging/stg_cliente.sql
@@ -8,7 +8,14 @@ with
               when tipo_cliente = 'PF' then 'Pessoa Física' end as tipo_cliente
           , date(data_inclusao) as data_inclusao
           , cpfcnpj as cpf
-          , date_diff(CURRENT_DATE(), data_nascimento, YEAR) as idade
+          , DATE_DIFF(CURRENT_DATE(), DATE(data_nascimento), YEAR) AS idade,  -- 🔥 Adicionado cálculo da idade
+            CASE 
+                WHEN DATE_DIFF(CURRENT_DATE(), DATE(data_nascimento), YEAR) < 18 THEN 'Menor de 18 anos'
+                WHEN DATE_DIFF(CURRENT_DATE(), DATE(data_nascimento), YEAR) BETWEEN 18 AND 25 THEN '18-25 anos'
+                WHEN DATE_DIFF(CURRENT_DATE(), DATE(data_nascimento), YEAR) BETWEEN 26 AND 35 THEN '26-35 anos'
+                WHEN DATE_DIFF(CURRENT_DATE(), DATE(data_nascimento), YEAR) BETWEEN 36 AND 50 THEN '36-50 anos'
+                ELSE 'Acima de 50 anos'
+            END AS faixa_etaria
           , endereco
           , cep  
            
