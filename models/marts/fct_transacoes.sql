b43e641e10e908f8c3dceb2652da657837758e13
gilberto <ant.gilbertosantos@gmail.com>
2025-02-07 21:57:28 -0300

    criando a fct_transacoes

---
+++ models/marts/fct_transacoes.sql
@@ -0,0 +1,34 @@
+with
+    transacoes as (
+        select *
+        from {{ ref('stg_transacoes') }}
+    )
+
+    , contas as (
+        select *
+        from {{ ref('stg_conta') }}
+    )
+
+    , dim_data as (
+        select data_completa
+        from {{ ref('dim_data') }}
+    )
+
+    , fct_transacoes as (
+        select
+            transacoes.transacao_id
+            , transacoes.conta_id as transacao_conta_id
+            , contas.conta_id as conta_conta_id
+            , transacoes.nome_transacao
+            , transacoes.valor_transacao
+            , contas.tipo_contas
+            , contas.saldo_total
+            , contas.data_abertura
+            , dim_data.data_completa
+        from transacoes
+        left join contas on transacoes.conta_id = contas.conta_id
+        left join dim_data on transacoes.data_transacao = dim_data.data_completa
+    )
+
+select *
+from fct_transacoes
