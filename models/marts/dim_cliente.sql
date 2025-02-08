db9097c2f34aa2030568663ad62abe4ef8f26c0d
gilberto <ant.gilbertosantos@gmail.com>
2025-02-08 15:23:59 -0300

    alterando coluna idade para faixa_etaria

--- models/marts/dim_cliente.sql
+++ models/marts/dim_cliente.sql
@@ -4,18 +4,25 @@ with
         from {{ref('stg_cliente')}}
 
     )
+    , contas as (
+        select *
+        from {{ ref('stg_conta') }}
+
+    )
     , dim_clientes as (
         select 
-            cliente_id
-            , nome_completo
-            , email
-            , tipo_cliente
-            , date(data_inclusao) as data_inclusao 
-            , cpf
-            , idade
-            , endereco
-            , cep
+            clientes.cliente_id
+            , contas.conta_id
+            , clientes.nome_completo
+            , clientes.email
+            , clientes.tipo_cliente
+            , date(clientes.data_inclusao) as data_inclusao 
+            , clientes.cpf
+            , clientes.faixa_etaria
+            , clientes.endereco
+            , clientes.cep
         from clientes
+        left join contas on clientes.cliente_id = contas.cliente_id
     )
 select *
 from dim_clientes
