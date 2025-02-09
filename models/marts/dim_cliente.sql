80032731f20d0dfbba346dc8003c57bb64f94b8b
gilberto <ant.gilbertosantos@gmail.com>
2025-02-09 15:16:25 -0300

    removendo as colunas email e endereco, e add coluna uf

--- models/marts/dim_cliente.sql
+++ models/marts/dim_cliente.sql
@@ -14,13 +14,11 @@ with
             clientes.cliente_id
             , contas.conta_id
             , clientes.nome_completo
-            , clientes.email
             , clientes.tipo_cliente
             , date(clientes.data_inclusao) as data_inclusao 
             , clientes.cpf
             , clientes.faixa_etaria
-            , clientes.endereco
-            , clientes.cep
+            , REGEXP_EXTRACT(clientes.endereco, r'([A-Z]{2})$') AS uf
         from clientes
         left join contas on clientes.cliente_id = contas.cliente_id
     )
